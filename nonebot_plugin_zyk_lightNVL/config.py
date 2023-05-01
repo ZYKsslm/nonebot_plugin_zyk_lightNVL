645a0fbbf319590e52cf6b901f2db2a4266542da
ZSSLM <3119964735@qq.com>
2023-05-01 21:24:52 +0800

    Update config.py

--- nonebot_plugin_zyk_lightNVL/config.py
+++ nonebot_plugin_zyk_lightNVL/config.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 from nonebot import get_driver
+from browser_cookie3 import load
+from selenium import webdriver
+from os import path
+
+
+class NoCookies(Exception):
+    def __str__(self):
+        return "未获取到cookies，请在浏览器中访问https://w.linovelib.com/search.html并重启bot！"
+
+# 获取cookies
+def reset_cookies():
+    cookies = load(domain_name="w.linovelib.com")
+    if not cookies:
+        raise NoCookies
+    return cookies
+
+cookies = reset_cookies()
 
 
 # 获取用户名
@@ -20,9 +37,11 @@ try:
 except AttributeError:
     retry_num = 50
 
-
-# 获取cookie
+# 获取搜索结果发送失败时重新发送的条数
 try:
-    cookie = get_driver().config.light_nvl_cookie
+    show_all = get_driver().config.light_nvl_show_all
 except AttributeError:
-    cookie = None
+    show_all = False
+
+
+driver = webdriver.PhantomJS(executable_path=path.join(path.abspath(path.dirname(__file__)), "phantomjs/bin/phantomjs.exe"))
