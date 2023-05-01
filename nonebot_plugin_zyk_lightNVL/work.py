854d77329facebf678bb791267a98562da95469a
ZSSLM <3119964735@qq.com>
2023-05-01 21:26:30 +0800

    :rocket: 重构逻辑

--- nonebot_plugin_zyk_lightNVL/work.py
+++ nonebot_plugin_zyk_lightNVL/work.py
@@ -1,10 +1,8 @@
 from httpx import AsyncClient
 import re
-from os import path
-from lxml import html
-from nonebot_plugin_htmlrender import html_to_pic
 from fake_useragent import UserAgent
 from time import time
+from .config import *
 
 
 async def get_checkcode():
@@ -18,7 +16,7 @@ async def get_checkcode():
         "rand": int(time())
     }
 
-    client = AsyncClient(headers=headers, timeout=None)
+    client = AsyncClient(headers=headers, timeout=None, cookies=cookies)
     r = await client.get(checkcode_api, params=param)
 
     return client, r.content
@@ -36,42 +34,44 @@ async def login(client: AsyncClient, username, password, checkcode):
     }
     resp = await client.post(url=login_api, data=data)
 
+    # 登录成功将会重定向
     if resp.status_code != 302:
         info = re.findall(r'<div class="aui-ver-form">(.*?)<br>', resp.text, re.S)[0].replace("\n", "")
         return False, info
-    else:
-        cookie_path = path.join(path.abspath(path.dirname(__file__)), "cookie.txt")
-        with open(file=cookie_path, mode="w", encoding="utf-8") as f:
-            f.write(resp.headers["set-cookie"])
-        return True, client
+
+    client.follow_redirects = True
+        
+    return True, client
 
 
 async def search(client: AsyncClient, name, retry_num):
 
     # 检索书名
-    url = "https://w.linovelib.com/S8/"
+    url = "https://w.linovelib.com/search.html"
     data = {
         "searchkey": name,
-        "searchtype": "all"
+    }
+    headers = {
+        "referer": "https://w.linovelib.com/search.html",
+        "oringin": "https://w.linovelib.com",
+        "method": "POST",
+        "User-Agent": UserAgent().random
     }
 
-    resp = await client.get(url=url, params=data)
+    resp = await client.post(url=url, data=data, headers=headers)
     info = resp.text
 
-    # 获取书名信息列表
-    book_list = re.findall(r'class="book-cover" alt="(.*?)">', info)
+    # 重定向（直接跳转到书页）
+    if "/novel/" in str(resp.url):
+        return "book_page", info
 
     # 没有重定向且有多个结果
-    if len(book_list) > 1:
-        href_list = [
-            "https://w.linovelib.com" + href
-            for href in re.findall(r'<a href="(.*?)" class="book-layout"', info)
-        ]
-        next_page = re.findall(r'</span><a href="(.*?)" class="next">', info)[0]
-        if next_page == "#":
+    elif '<meta name="robots" content="noindex,nofollow">' not in info:
+        book_list, href_list, next_page = search_parse(info)
+        if (next_page is None) or (show_all is False):
             return book_list, href_list
 
-        # 不止一页
+        # 不止一页，根据retry_num选择需要多少页
         if retry_num < 30:
             page = 1
         else:
@@ -79,10 +79,9 @@ async def search(client: AsyncClient, name, retry_num):
             if retry_num % 30 != 0:
                 page += 1
 
-        for _ in range(page-1):
+        for _ in range(page - 1):
             if next_page == "#":
                 break
-            next_page = "https://w.linovelib.com" + next_page
             info = (await client.get(url=next_page)).text
             # 获取书名信息列表
             books = re.findall(r'class="book-cover" alt="(.*?)">', info)
@@ -92,45 +91,45 @@ async def search(client: AsyncClient, name, retry_num):
             ]
             book_list += books
             href_list += hrefs
-
+            
             next_page = re.findall(r'</span><a href="(.*?)" class="next">', info)[0]
 
         return book_list, href_list
 
-    # 重定向（直接跳转到书页）
-    elif "/novel/" in str(resp.url):
-        pattern = re.compile(r'<meta property="og:title" content="(.*?)" />')
-        book_name = [re.findall(pattern, info)[0]]
-
-        return book_name, [str(resp.url)]
-
+    # 无结果
     else:
         return False
 
 
-async def get_content(client: AsyncClient, book_url):
-    book_info = (await client.get(url=book_url)).text
+def search_parse(res):
+    # 获取书名信息列表
+    book_list = re.findall(r'alt="(.*?)"', res, re.S)
+    href_list = [
+        "https://w.linovelib.com" + href
+        for href in re.findall(r'<li class="book-li"><a href="(.*?)" class="book-layout">', res)
+    ]
+    next_page = re.findall(r'</span><a href="(.*?)" class="next">', res)[0]
+    if next_page == "#":
+        return book_list, href_list, None
+    else:
+        return book_list, href_list, "https://w.linovelib.com" + next_page
 
-    content_pattern = re.compile(r'(<div class="module module-merge">.*?)<div class="module">', re.S)
-    index_pattern = re.compile(r'<li class="btn-group-cell"><a href="(.*?)"')
-    info_pattern = re.compile(r'<div id="bookDetailWrapper" class="module module-merge book-detail-x">.*?</span>\n</p>\n\n</div>\n</div>', re.S)
 
-    info = re.findall(info_pattern, book_info)[0]
+async def get_content(book_info):
+
+    content_pattern = re.compile(r'(<!DOCTYPE html>.*)<style>.page-fans', re.S)
+ 
     content = re.findall(content_pattern, book_info)[0]
-    index_url = "https://w.linovelib.com" + re.findall(index_pattern, book_info)[0]
-
-    try:
-        book_id = re.findall(r'https://w.linovelib.com/novel/(\d+).html', book_url)[0]
-    except IndexError:
-        book_id = re.findall(r'acode=i_(\d+)', book_url)[0]
 
     html_path = path.join(path.abspath(path.dirname(__file__)), "template.html")
-    with open(html_path, "r", encoding="utf-8") as h:
-        template = h.read()
-    content = template.format(content=content, info=info)
-    pic = await html_to_pic(html=content)
+    with open(html_path, "w+", encoding="utf-8") as h:
+        h.write(content)
 
-    return pic, index_url, book_id
+    driver.get(f'file:///{html_path}')
+    driver.implicitly_wait(1)
+    pic = driver.get_screenshot_as_png()
+
+    return pic
 
 
 async def get_bookcase(client: AsyncClient):
@@ -138,6 +137,7 @@ async def get_bookcase(client: AsyncClient):
 
     if "login" in str(resp.url):
         return False
+
     case_info = resp.text
     book_pattern = re.compile(r'<h4 class="book-title">(.*?)</h4>')
     url_pattern = re.compile(r'<a href="(.*?)" class="mybook-to-detail">')
